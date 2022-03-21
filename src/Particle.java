e259dfd5c19107032866188b06d01feb76c5ccb9
SamiFAHIM <101048000+SamiFAHIM@users.noreply.github.com>
2022-03-21 09:55:48 +0100

    Add files via upload

--- src/Particle.java
+++ src/Particle.java
@@ -7,7 +7,7 @@ public class Particle {
 
     int charge;
     Point2D.Double speed;
-    Point position;
+    Point2D.Double position;
     int accelerationY;
     boolean isVisible;
     int amplitudeSpeed;
@@ -15,21 +15,20 @@ public class Particle {
     int speedFrequency;
 
     public Particle(int x, int y, int charge, Image img, int frequency, int amplitude) {
-        this.position = new Point(x, y);
+        this.position = new Point2D.Double(x, y);
         this.charge = charge;
         this.img = img;
 
         this.speedFrequency = frequency;
         this.amplitudeSpeed = amplitude;
 
-        this.speed = new Point2D.Double(5, 0);
+        this.speed = new Point2D.Double(1, 0);
     }
 
     public void move(int bufferWidth) {
-//        speed.y = Math.cos(speedFrequency * );
-        
-        position.x = (int)(position.x + speed.x);
-        position.y = (int)(position.y + speed.y);
+		position.x = (position.x + speed.x);
+		speed.y = (amplitudeSpeed*Math.cos(2*Math.PI*speedFrequency *MainWindow.FrameCounter));
+        position.y = (position.y + speed.y);
 
         if (position.x >= bufferWidth || position.x <= 0) {
             speed.x = -speed.x;
